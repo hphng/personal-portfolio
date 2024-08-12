722087a414c40a7c8aa647736efb2349ad0a7fee
Huy Phung <huyphung3103@gmail.com>
2024-08-11 23:51:29 -0400

    add styling projects

--- src/pages/ProjectPage/index.jsx
+++ src/pages/ProjectPage/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './index.css'
+import {Button, Card} from 'react-bootstrap'
 import huyVideo from '../../assets/projects/huy.mp4'
 import finbud from '../../assets/projects/finbud.png'
 import musicApp from '../../assets/projects/music-app.png'
@@ -25,14 +26,14 @@ const ProjectPage = () => {
     {
       title: 'Huy',
       description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi reiciendis magni, possimus illo voluptates adipisci sint ad dolorem, fugiat labore quas dolores, asperiores eius nihil. Excepturi blanditiis vero expedita mollitia?',
-      source: businessCard,
+      source: chatBox,
       live: 'https://finbud.netlify.app/',
       github: ''
     },
     {
       title: 'Huy',
       description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi reiciendis magni, possimus illo voluptates adipisci sint ad dolorem, fugiat labore quas dolores, asperiores eius nihil. Excepturi blanditiis vero expedita mollitia?',
-      source: chatBox,
+      source: businessCard,
       live: 'https://finbud.netlify.app/',
       github: ''
     },
@@ -65,17 +66,20 @@ const ProjectPage = () => {
                     {/* <img className="project-image" src={project.source} alt="project-image" /> */}
                   </div>
                 </a>
-                <div className="project-info">
-                  <h3 className='project-title'>{project.title}</h3>
-                  <p className="project-description">{project.description}</p>
-                  <a href={project.live}>Live</a>
-                  <a href={project.github}>Learn more</a>
-                </div>
+
+                <Card className="project-info">
+                  <Card.Body>
+                    <Card.Title className='project-title'>{project.title}</Card.Title>
+                    <Card.Subtitle className="project-subtitle text-light">Card Subtitle</Card.Subtitle>
+                    <Card.Text className="project-description">{project.description}</Card.Text>
+                    <Card.Link href={project.live} as="button" className='project-button project-live'>Live</Card.Link>
+                    <Card.Link href={project.github} as="button" className='project-button project-github'>Learn more</Card.Link>
+                  </Card.Body>
+                </Card>
               </div>
             ) 
           )
         })}
-        {/* <video className="video" src={huyVideo} controls ></video> */}
     </section>
   )
 }
