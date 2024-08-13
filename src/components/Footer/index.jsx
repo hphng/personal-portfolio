05ca2b84e578fa74516c2adeed3a499dcad32391
Huy Phung <huyphung3103@gmail.com>
2024-08-13 15:47:05 -0400

    add social media and animation button to footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './index.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faSquareGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faChevronUp } from '@fortawesome/free-solid-svg-icons'
+import { Button } from 'react-bootstrap'
 const Footer = () => {
   return (
     <footer className='footer-container'>
-        <p>© 2024 - All rights reserved</p>
-        <h1>Footer</h1>
+      <Button href="#home" variant='danger'>
+          <FontAwesomeIcon icon={faChevronUp} />
+      </Button>
+      <div className="social-media">
+        <a href="https://www.linkedin.com/in/huyphung2025/">
+            <FontAwesomeIcon className="icon" icon={faLinkedin} />
+        </a>
+        <a href="mailto:huyphung3103@gmail.com">
+            <FontAwesomeIcon className="icon" icon={faEnvelope} />
+        </a>
+        <a href="https://github.com/hphng">
+            <FontAwesomeIcon className="icon" icon={faSquareGithub} />
+        </a>
+      </div>
+      <p>Henry Phung - © 2024</p>
     </footer>
   )
 }
