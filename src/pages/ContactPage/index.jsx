b167f0dedbb8268aa87fe8d0fd2ec8165980ccdf
Huy Phung <huyphung3103@gmail.com>
2024-10-27 22:27:58 -0400

    fix the contact page

--- src/pages/ContactPage/index.jsx
+++ src/pages/ContactPage/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import './index.css'
 import { Form, Button, Alert } from 'react-bootstrap'
 
@@ -36,45 +36,59 @@ const ContactPage = () => {
         setMessage('');
         setName('');
     };
+    useEffect(() => {
+        if (alert.show) {
+            const timer = setTimeout(() => {
+                setAlert({ show: false, variant: '', message: '' });
+            }, 2000);
+
+            // Clear timeout if the component unmounts or alert state changes
+            return () => clearTimeout(timer);
+        }
+    }, [alert]);
+
     return (
         <section className='contact-container' id='contact'>
             <header className='contact-header'>Contact</header>
-            {/* {alert.show && (
-                <Alert className="alert" variant={alert.variant} onClose={() => setAlert({ show: false })} dismissible>
-                    {alert.message}
-                </Alert>
-            )} */}
-            <Form onSubmit={handleSubmit} className="contact-form">
-                <Form.Group controlId='formBasicMessage'>
-                    <Form.Control
-                        type='text'
-                        placeholder='Name'
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                </Form.Group>
-                <Form.Group controlId='formBasicEmail'>
-                    <Form.Control
-                        type='email'
-                        placeholder='Email'
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </Form.Group>
+            <div className='contact-form-container'>
+                <h1>If you want to further collaborate, </h1>
+                {alert.show && (
+                    <Alert className="alert" variant={alert.variant} onClose={() => setAlert({ show: false })} dismissible>
+                        {alert.message}
+                    </Alert>
+                )}
+                <Form onSubmit={handleSubmit} className="contact-form">
+                    <Form.Group controlId='formBasicMessage'>
+                        <Form.Control
+                            type='text'
+                            placeholder='Name'
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId='formBasicEmail'>
+                        <Form.Control
+                            type='email'
+                            placeholder='Email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </Form.Group>
 
-                <Form.Group controlId='formBasicMessage'>
-                    <Form.Control
-                        as='textarea'
-                        rows={6}
-                        value={message}
-                        placeholder='Message'
-                        onChange={(e) => setMessage(e.target.value)}
-                    />
-                </Form.Group>
-                <Button className='btn btn-danger' type='submit'>
-                    Submit
-                </Button>
-            </Form>
+                    <Form.Group controlId='formBasicMessage'>
+                        <Form.Control
+                            as='textarea'
+                            rows={6}
+                            value={message}
+                            placeholder='Message'
+                            onChange={(e) => setMessage(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Button className='btn btn-danger' type='submit'>
+                        Submit
+                    </Button>
+                </Form>
+            </div>
         </section>
     )
 }
