5674d64e984523222446f824e03f8b55cea3ed7b
Huy Phung <huyphung3103@gmail.com>
2026-03-02 11:23:36 -0500

    update job content

--- src/pages/ExperiencePage/index.jsx
+++ src/pages/ExperiencePage/index.jsx
@@ -5,31 +5,54 @@ const ExperiencePage = () => {
   const work = [
     {
       id: "1",
-      date: "2024",
-      title: "Software Engineer Intern",
-      location: "Remote - USA",
-      company: "Birdie AI Company - Startup",
+      date: "Dec 2025 - Present",
+      title: "Software Engineer",
+      location: "Seattle, Washington",
+      company: "Venera",
       description: [
-        "Developed an AI-driven prompt search system, with React and TypeScript, implementing Debounce techniques to reduce API calls and fuzzy string matching to enhance response accuracy and search effiency",
-        "Revamped the codebase system architecture to support more new product offrings and reducing bugs over 30%",
-        "Deployed backend infrastructure on AWS, utilizing AWS lambda, API Gateway, and SAM (Serverless Application Model) to optimize performance, reducing server costs by 45% through optimized resource allocation",
-        " Automated the deployment pipeline by creating a Bash Script to streamline backend production updates, reducing manual deployment time by 60% and minimizing errors"
+        "Developed a AI-powered personal health companion using Django, PostgreSQL, and Docker to deliver APIs for biometrics analysis, OCR, and Apple Health ingestion, supporting about 1500+ monthly health evaluations",
+        "Integrated a unified GPT orchestration layer wrapping Azure OpenAI to handle streaming chat, tool calls, and system-prompt consistency, improving model response stability by 40% across OCR and health-analysis workflows",
+        "Engineered a CI/CD pipeline using Github Action and Microsoft Azure, automated Docker image build to ACR and Linux VM, eliminated manual intervention, and reduced deployment time from 30 minutes to 5 minutes",
       ]
     },
     {
       id: "2",
-      date: "2024",
-      title: "Software Engineer Team Lead",
-      location: "Chicago, Illinois",
-      company: "Finbud AI Compny - Startup",
+      date: "May 2025 - Dec 2025",
+      title: "AI Engineer Scholar",
+      location: "Seattle, Washington",
+      company: "Amazon Web Services (AWS) AI Engineer Scholar",
       description: [
-        "Developed a full-stack fiancial app using OpenAI APIs, Vue JS and Netlify to track stock on real-time, getpersonalized trade suggestions, and get fiancial guidance by AI Chatbot",
-        "Design a system with multi-threaded authentication flw using Passport.js, reducing authentication latency by 25%, and ensuring 99% reliability through integration testing of OAuth and JWT verifiation",
-        "Led a team of 15 to integrate Alpha Vantage and Polyon.io APIs, providing real-time fiancial market data of companies, which attracted key investment partner, and boosted user adoption by 40%",
+        "Constructed an intelligent document-querying system with AWS Bedrock, S3, Aurora Serverless vector indexing, and VPC-isolated, enabling multi-sentence semantic search over 500+ PDFs with 2–5s retrieval latency",
+        "Built an end-to-end retrieval pipeline: PDF ingestion, chunking, embedding generation, vector indexing, retrieval, prompt augmentation, and LLM inference, achieving 92% query accuracy for multi-sentence searches",
+        "Implemented prompt-classifiation guardrails to automatically detect and fiter unsafe or out-of-domain queries"
       ]
     },
     {
       id: "3",
+      date: "Aug 2025 - Dec 2025",
+      title: "Software Engineer Intern",
+      location: "Valdosta, Georgia",
+      company: "South Georgia Pecan Company",
+      description: [
+        "Designed an internal operations platform using React, Tailwinds, and MUI to replace paper-based workflws, enabling 100+ warehouse staf to digitally submit forms, reducing operational cost by $55K+ annually",
+        "Deployed a Docker-based internal registry and CI/CD pipeline for on-premises warehouse infrastructure, standardizing backend deployment on the warehouse network with health-check driven rollback",
+        "Built a small C++ utility and Bash scripting on Linux to process and aggregate server operational logs, focusing on file I/O, memory-safe data structures, and efficient parsing for internal monitoring"
+      ]
+    },
+    {
+      id: "4",
+      date: "2024",
+      title: "Software Engineer Intern",
+      location: "Hanoi, Vietnam",
+      company: "Finbud",
+      description: [
+        "Developed a full-stack financial app using Nodejs, OpenAI APIs, Vuejs, and Netlify for beginner and retail investors to get financial guidance by AI Chatbot, track stock on real time, and learn through financial quizzes",
+        "Architected a RAG pipeline reducing hallucination rates 35% in sensitive financial recommendations",
+        "Led a team of 5 to integrate Alpha Vantage and Polyon.io APIs, providing real-time financial market data of 200 companies, which attracted key investment partner that funded $5k+, and boosted user adoption by 40%"
+      ]
+    },
+    {
+      id: "5",
       date: "2023",
       title: "Software Engineer Intern",
       location: "Hanoi, Vietnam",
